c4a4484e52cecf09bf14666d50b279c0f1299b3b
xndcmd <xndcmd@gmail.com>
2022-03-10 19:33:47 -0800

    Added more robust map functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,6 +110,53 @@ function searchApi(state) {
 }
 
 
+
+// add marker on map
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+
+// add a sized circle to map
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+
+// add a polygon to map
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+
+// add pop-ups to the map object, displaying text when you click the defined object.
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+
+// for adding more layers (extra info to popups than just text, loads on refresh)
+var popup = L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("Hello, Welcome to the Map, A-Team! I am a standalone popup. WHOOOOPAHH")
+    .openOn(map);
+
+    
+// click anywhere on map for the popup
+    var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+map.on('click', onMapClick);
+
+
 // Connects to MapBox tile
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
